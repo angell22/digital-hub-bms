36eaeab99700faeb906a1ce7b073e1e4f7e76533
angell22 <angellnathaline0605@gmail.com>
2023-03-30 21:50:25 +0800

    Add create and delete action to manage account

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'rack/method_override'
 
+use Rack::MethodOverride
 
 require File.expand_path(File.join('controllers', 'application_controller'))
 require File.expand_path(File.join('models', 'rooms'))
--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,26 +1,90 @@
 class ApplicationController < Sinatra::Base 
+
     enable:sessions
+
     # Set folder for templates to ../views, but make the path absolute
     set :views, File.expand_path('../views', '_FILE_')
 
-    ## index page
+    ## index page ##
     get '/' do 
         erb :index
     end 
 
+    ## USER ACTION ##
     # login page
     post '/login' do
-        if session[logged_in] == true
-            @user = User.find_by(email: params[:email])
-            session[:user_id] = @user.id
-            
+        username = params[:username]
+        password = params[:password]
         
-        else
-            @user = User.find_by(email: params[:email])
-            session[:user_id] = @user.id
-            redirect "/profile"
+        user = User.find_by(username: username, password_digest: password)
 
+        if user
+            session[:user_id] = user.id
+            session[:logged_in] = true
+
+            if user.role==false
+                redirect '/admin'
+            else
+                redirect '/home'
+            end
+
+        else
+            erb :index, locals:{error: 'Invalid!'}
         end
+
+    end
+
+    #user page
+    get '/home' do
+        if session[:logged_in]
+            #find logged in user data
+            @users = User.where(id: session[:user_id])
+            @rooms = Room.all
+            erb :home
+        else
+            redirect '/'
+        end
+    end
+
+    #logout page
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
+  # Direct to edit user account
+    get '/edit/user/:id' do
+        @user = User.find(params[:id])
+        erb :edit   
+    end
+  
+  
+
+    #update account
+    post '/update/user/:id' do
+        username = params[:username]
+        password_digest = params[:password]
+        user_id = params[:id]
+        
+        user = User.find_by(id: user_id)
+
+        if user.update(username: username, password_digest: password_digest)
+            if user.role
+                redirect '/admin'
+            else
+                redirect '/home'
+            end
+        else
+            erb :error, locals:{message: 'Failed to update profile. Try again.'}
+        end
+    end
+
+    #delete user account 
+    get '/delete/user/:id' do
+        @user = User.find(params[:id])
+        @user.destroy
+
+        redirect '/'
     end
 
     # registration page
@@ -31,9 +95,9 @@ class ApplicationController < Sinatra::Base
     #registration form
     post '/register' do
         username = params[:username]
-        password = params[:password]
+        password_digest = params[:password]
 
-        user = User.new(username: username, password:password)
+        user = User.new(username: username, password_digest:password_digest)
 
         user.save
         redirect '/'
@@ -41,7 +105,40 @@ class ApplicationController < Sinatra::Base
 
     # retrieve user's information from database
     get 'users/:id' do
-        @user = User.find(params[:id])
+        @users = User.find(params[:id])
         erb :user
     end
+
+    ## ROOM ACTION ##
+    #add room
+    get '/add_room' do
+        erb :add_room
+    end
+
+    #create room
+    post '/add' do
+        room_name= params[:room_name]
+        rate= params[:rate]
+        capacity= params[:capacity]
+
+        room= Room.new(room_name: room_name, rate: rate, capacity: capacity)
+
+        room.save
+        redirect '/admin'
+    end
+
+    #view room
+    get '/room_list' do
+        @rooms = Room_all
+        erb :room_list
+    end
+
+    ## ADMIN ACTION ##
+    #admin page
+    get '/admin' do
+        erb :admin
+    end
+
+    ## BOOKING ACTION ##
+
 end
